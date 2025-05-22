f2ad0b2eb3870c656779579dc3072f8bb82594fb
Shreyas Ladhe <shreyasladhe.23@gmail.com>
2025-05-22 20:13:48 +0530

    verify link line

--- src/auth/view/supabase/supabase-verify-view.jsx
+++ src/auth/view/supabase/supabase-verify-view.jsx
@@ -15,7 +15,7 @@ export function SupabaseVerifyView() {
       <FormHead
         icon={<EmailInboxIcon />}
         title="Please check your email!"
-        description={`We've emailed a 6-digit confirmation code. \nPlease enter the code in the box below to verify your email.`}
+        description={`We've emailed a confirmation link to you for your email address. \nPlease check your email and click the link to verify your email.`}
       />
 
       <FormReturnLink href={paths.auth.supabase.signIn} sx={{ mt: 0 }} />
